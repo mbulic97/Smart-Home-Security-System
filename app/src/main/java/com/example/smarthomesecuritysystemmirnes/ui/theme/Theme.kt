b47d75b1bbb00001083949fdc15db8891f3ccca6
mbulic97 <mirnesbulic32@gmail.com>
2025-09-27 01:04:53 +0200

    Project finished v1

--- app/src/main/java/com/example/smarthomesecuritysystemmirnes/ui/theme/Theme.kt
+++ app/src/main/java/com/example/smarthomesecuritysystemmirnes/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.smarthomesecuritysystemmirnes.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
